ba0cf7856a9c4f6f0064c45456d5b39b82613cac
Katie Kujala <katie_kujala@harvard.edu>
2016-11-10 13:11:06 -0500

    seeded, rolled back a migration

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -101,26 +101,4 @@ class BookController extends Controller
         //
     }
 
-
-    /**
-	* This was example code I wrote in Lecture 7
-    * It shows, roughly, what a controller action for your P3 might look like
-    * It is not at all related to the Book resource.
-	*/
-    public function getLoremIpsumText(Request $request)
-    {
-
-        # Validate the request....
-
-        # Generate the lorem ipsum text
-        $howManyParagraphs = $request->input('howManyParagraphs');
-
-        # Logic...
-        $loremenator = \SBuck\Loremenator();
-        $text = $loremenator->getParagraphs($howManyParagraphs);
-
-        # Display the results...
-        return view('lorem')->with(['text', $text]);
-
-    }
 }
